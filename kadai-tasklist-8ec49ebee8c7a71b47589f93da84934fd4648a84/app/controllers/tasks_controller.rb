d8bedfdcb14f590296ddcadee2e5089185bd51f4
utsugikanazawa <jonathan.arma.sute@gmail.com>
2017-04-26 04:59:56 +0000

    edit

--- kadai-tasklist-8ec49ebee8c7a71b47589f93da84934fd4648a84/app/controllers/tasks_controller.rb
+++ kadai-tasklist-8ec49ebee8c7a71b47589f93da84934fd4648a84/app/controllers/tasks_controller.rb
@@ -1,6 +1,19 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
 
+  before_action :correct_user, only: [:destroy]
+  
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to root_url
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end
+  end
   def create
     @task = current_user.tasks.build(task_params)
     if @task.save
@@ -14,9 +27,13 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task_params.destroy
+    @task.destroy
     flash[:success] = 'メッセージを削除しました。'
     redirect_back(fallback_location: root_path)
+  end
+    
+  def edit
+    @task = Task.find(params[:id]) 
   end
 
 
